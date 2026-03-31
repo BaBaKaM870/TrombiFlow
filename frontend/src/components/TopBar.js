676aaec2c379ec7b629dae9aee3449fe7f2fa513
Jonas-Mukuri-Maka <zmukuri@gmail.com>
2026-03-31 13:51:13 +0200

    Ajoute de Pages React: Tableau de Bord et Class

---
+++ frontend/src/components/TopBar.js
@@ -0,0 +1,10 @@
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbar-title">
+        Tableau de <span>bord</span>
+      </div>
+      <button className="session-btn">＋ Session active</button>
+    </div>
+  )
+}
