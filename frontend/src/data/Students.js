c4bac71ba2804d731f0d71994efb655a9a13a5e7
IGUENIlyes <l_igueni@estin.dz>
2026-04-01 13:25:22 +0200

    Amélioration export PDF trombinoscope

--- frontend/src/data/Students.js
+++
@@ -1,6 +0,0 @@
-export const STUDENTS = [
-  { initials: 'FL', name: 'Félix Laurent', email: 'felix.laurent@etudiant.fr', classe: '3A' },
-  { initials: 'AM', name: 'Amélie Martin', email: 'amelie.martin@etudiant.fr', classe: '2B' },
-  { initials: 'LB', name: 'Lucas Bernard', email: 'lucas.bernard@etudiant.fr', classe: '1C' },
-  { initials: 'CS', name: 'Chloé Simon', email: 'chloe.simon@etudiant.fr', classe: 'TS1' },
-]
